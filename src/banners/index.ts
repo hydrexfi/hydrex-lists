1558bf3de5b925432267d2e2b61f3fe75c137e15
Garrett Lee <garr.c.lee@gmail.com>
2026-05-25 10:17:51 -0500

    banners

--- src/banners/index.ts
+++ src/banners/index.ts
@@ -2,13 +2,22 @@ import { Banner } from "../types";
 
 export const Banners: Banner[] = [
     {
-        title: "Send.Trade",
-        description: "Send.Trade has revamped",
-        mobileBannerImage: "https://raw.githubusercontent.com/hydrexfi/hydrex-lists/staging/assets/banners/send-new-mobile.png",
-        desktopBannerImage: "https://raw.githubusercontent.com/hydrexfi/hydrex-lists/staging/assets/banners/send-new-desktop.png",
-        externalUrl: "https://send.trade",
+        title: "Bankr Skills Live",
+        description: "Hydrex Skills Live on Bankr",
+        mobileBannerImage: "https://raw.githubusercontent.com/hydrexfi/hydrex-lists/staging/assets/banners/bankr-mobile.png",
+        desktopBannerImage: "https://raw.githubusercontent.com/hydrexfi/hydrex-lists/staging/assets/banners/bankr-desktop.png",
+        internalUrl: "/pools",
         isLive: true,
         placementNumber: 1,
+    },
+    {
+        title: "Send.Trade",
+        description: "Send.Trade has revamped",
+        mobileBannerImage: "https://raw.githubusercontent.com/hydrexfi/hydrex-lists/staging/assets/banners/send-early-mobile.png",
+        desktopBannerImage: "https://raw.githubusercontent.com/hydrexfi/hydrex-lists/staging/assets/banners/send-early-desktop.png",
+        externalUrl: "https://send.trade",
+        isLive: true,
+        placementNumber: 2,
     },
     {
         title: "Hydrex Academy",
@@ -17,15 +26,15 @@ export const Banners: Banner[] = [
         desktopBannerImage: "https://raw.githubusercontent.com/hydrexfi/hydrex-lists/staging/assets/banners/academy-desktop.png",
         externalUrl: "https://academy.hydrex.fi",
         isLive: true,
-        placementNumber: 2,
-    },
-    {
-        title: "Liquidity Provisioning",
-        description: "Work with us to provide liquidity.",
-        mobileBannerImage: "https://raw.githubusercontent.com/hydrexfi/hydrex-lists/staging/assets/banners/liquidity-mobile.png",
-        desktopBannerImage: "https://raw.githubusercontent.com/hydrexfi/hydrex-lists/staging/assets/banners/liquidity-desktop.png",
-        externalUrl: "https://docs.google.com/forms/d/e/1FAIpQLScR82qfEFmSysrbrSESiJr9wM1ZAWUVnYJIouS2xBio5iH0zg/viewform?usp=sharing&ouid=105771834148534931546",
-        isLive: true,
         placementNumber: 3,
     },
+    // {
+    //     title: "Liquidity Provisioning",
+    //     description: "Work with us to provide liquidity.",
+    //     mobileBannerImage: "https://raw.githubusercontent.com/hydrexfi/hydrex-lists/staging/assets/banners/liquidity-mobile.png",
+    //     desktopBannerImage: "https://raw.githubusercontent.com/hydrexfi/hydrex-lists/staging/assets/banners/liquidity-desktop.png",
+    //     externalUrl: "https://docs.google.com/forms/d/e/1FAIpQLScR82qfEFmSysrbrSESiJr9wM1ZAWUVnYJIouS2xBio5iH0zg/viewform?usp=sharing&ouid=105771834148534931546",
+    //     isLive: true,
+    //     placementNumber: 3,
+    // },
 ];
